484add27b9c1313e8f50ba33719b06456d44a761
rdpy56952-wq <rdpy56952@gmail.com>
2026-01-29 11:02:25 +0530

    without3rdvariable

--- java/suyash sir/swapwithout.java
+++ java/suyash sir/swapwithout.java
@@ -1,3 +1,16 @@
-public class swapwithout {
+import java.util.Scanner;
 
+public class swapwithout {
+    public static void main(String[] args) {
+        System.out.println("Enter the value of a");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        System.out.println("Enter the value of b ");
+        int b = sc.nextInt();
+        a = a + b - a;
+        b = a + b - b;
+        System.out.println("The value of a is " + a + " and value of b is " + b);
+
+    }
 }
