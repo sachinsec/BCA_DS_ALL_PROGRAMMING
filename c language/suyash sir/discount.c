1e801889e9ad891970e31746e4a676ef204f24e7
rdpy56952-wq <rdpy56952@gmail.com>
2026-02-05 10:56:10 +0530

    dis

--- c language/suyash sir/discount.c
+++ c language/suyash sir/discount.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 int main(){
-int am
+int am;
+printf("Enter amount: ");
+scanf("%d",&am);
+if (am>10000)
+{
+     float dis=(am*10)/100;   
+    printf("bill prize = %f ",am-dis);
+}
+else
+{
+    float dis=(am*5)/100;   
+    printf("bill prize = %f ",am-dis);
+}
+
+
     return 0;
 }
