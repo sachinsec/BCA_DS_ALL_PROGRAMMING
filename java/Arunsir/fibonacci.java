cec4a1e69d747390faba3620b9ffbd8f60605545
rdpy56952-wq <rdpy56952@gmail.com>
2026-02-10 15:41:26 +0530

    fibo

--- java/Arunsir/fibonacci.java
+++ java/Arunsir/fibonacci.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class fibonacci {
     public static void main(String[] args) {
-        int a, b = 1, c;
-        for (a = 0; a <= 10; a++) {
+        int a = 0, b = 1, c;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many term ");
+        int d = sc.nextInt();
+
+        System.out.println("Fibonacci Series:");
+        System.out.println("0");
+        System.out.println(b);
+
+        for (int i = 1; i < d; i++) {
             c = a + b;
             System.out.println(c);
-            b = a;
+            a = b;
             b = c;
         }
     }
