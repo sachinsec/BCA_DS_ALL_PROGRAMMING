020689a516cc2c1147c69a809ad4b6db1d109178
sachinsec <rdpy625@gmail.com>
2026-02-21 02:37:05 -0800

    space detecting

--- java/self/practice2.java
+++ java/self/practice2.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class practice2 {
     public static void main(String[] args) {
-        String na ="Sachin Prajapat";
+        String na;
+        Scanner sc=new Scanner(System.in);
+        na=sc.nextLine();
+        int co=0;
         for(int i = 0;i<na.length();i++){
-            System.out.println(na.charAt(i));
-        }        
+           if (na.charAt(i)==' ') {
+            co++;
+           }
+        }  
+        System.out.println("White space = "+co);      
     }}
