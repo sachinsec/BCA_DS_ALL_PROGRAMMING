66171ead2190c34958724b3467b2bd8b6ac19f25
sachinsec <rdpy625@gmail.com>
2026-02-21 01:35:00 -0800

    for checking

--- java/self/practice2.java
+++ java/self/practice2.java
@@ -1,7 +1,7 @@
 public class practice2 {
     public static void main(String[] args) {
-        String name ="Sachin Prajapat";
-        for(int i = 0;i<name.length();i++){
-            System.out.println(name.charAt(i));
+        String na ="Sachin Prajapat";
+        for(int i = 0;i<na.length();i++){
+            System.out.println(na.charAt(i));
         }        
     }}
