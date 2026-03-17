cd7a945d33176156cb8e76d98929827d726c5f37
sachinsec <rdpy625@gmail.com>
2026-03-17 16:11:44 +0530

    16-3-26

--- java/self/doubt.java
+++ java/self/doubt.java
@@ -1,25 +1,51 @@
 import java.util.Scanner;
-class parent{
-    public void display(){
-        System.out.println("I'm parents class");
+class roman{
+    int i=1,v=5,x=10,l=50,c=100,d=500,m=1000;
+    int result=0,check=0;
+    String n;
+    public String get(String s){
+     return n=s.toLowerCase();
+
     }
-}
-class child extends parent{
-    public void display(){
-        System.out.println("I'm child class");
+    public int right(){
+        for(int j =0;j< n.length();j++){
+       if (n.charAt(j)=='m') {
+           check = m;
+       }
+       else if (n.charAt(j)=='d'){
+           check = d;
+       }
+       else if (n.charAt(j)=='c'){
+           check = c;
+       }
+       else if (n.charAt(j)=='l'){
+           check = l;
+       }
+       else if (n.charAt(j)=='x'){
+           check = x;
+       }
+       else if (n.charAt(j)=='v'){
+           check = v;
+       }
+       else if (n.charAt(j)=='i'){
+           check = i;
+       }
+      result += check;
+   }
+   return result;
     }
-}
-class child1 extends parent{
-    public void display(){
-        System.out.println("I'm child1 class");
-    }
-}
-public class doubt {
-    public static void main(String[] args) {
-        child c = new child();
-        child1 c1 = new child1();
-        c1.display();
-        c.display();
+} 
+
+
+public class doubt{
+    public static void main(final String[] args) {
+        
+        String s="MCMXCIV";
+
+        roman obj=new roman();
+        obj.get(s);
+
+    System.out.println(obj.right());
 
     }
 }
