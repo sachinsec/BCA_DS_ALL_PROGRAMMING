e61fbb3c36e4add7c0c2292631dd2dd5b00c8679
sachinsec <rdpy625@gmail.com>
2026-04-08 14:03:57 +0530

    Add initial implementation of logest_common_prefix class with main method and input handling

---
+++ java/codingblocks/logest_common_prefix.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class logest_common_prefix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s=sc.nextLine();
+        int n = s.length();
+        String [] str = new String[n];
+        System.out.println(n);
+
+    }
+}
